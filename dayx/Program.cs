4d3c978334674d79c62490e10f44d1b3368c2ce7
manikanta0709 <manibunny0718@gmail.com>
2022-06-18 06:07:00 +0530

    added: added uc5

--- dayx/Program.cs
+++ dayx/Program.cs
@@ -3,13 +3,15 @@ Console.WriteLine("welcome to employee wage computation program");
 Random random = new Random();
 int res = random.Next(0, 2);
 Console.WriteLine(res);
-const int wageperhour = 20, dayhour = 8, parttimehour=4;
+const int wageperhour = 20, dayhour = 8, parttimehour=4,workingdaypermonth=20;
 switch (res)
 {
     case 1:
         Console.WriteLine("present");
         Console.WriteLine("dailywage:" + wageperhour * dayhour);
         Console.WriteLine("parttime wage:" + wageperhour * parttimehour);
+        Console.WriteLine("full time :" + (wageperhour * dayhour) * workingdaypermonth);
+        Console.WriteLine("parttime:" + (wageperhour * parttimehour) * workingdaypermonth);
         break;
     default:
         Console.WriteLine("absent");
