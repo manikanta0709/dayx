0941fbfb5855106038765a3f83d0575fbd4a4a90
manikanta0709 <manibunny0718@gmail.com>
2022-06-18 05:45:50 +0530

    added:added uc3

--- dayx/Program.cs
+++ dayx/Program.cs
@@ -3,11 +3,12 @@ Console.WriteLine("welcome to employee wage computation program");
 Random random = new Random();
 int res = random.Next(0, 2);
 Console.WriteLine(res);
-const int wageperhour= 20, dayhour = 8;
+const int wageperhour = 20, dayhour = 8, parttimehour=4;
 if(res==1)
 {
     Console.WriteLine("present");
     Console.WriteLine("dailywage:"+ (wageperhour * dayhour));
+    Console.WriteLine("parttime wage:" + (wageperhour * parttimehour));
 }
 else
 {
