75907ef87e76d4ae302341d798608a7dc69ad462
manikanta0709 <manibunny0718@gmail.com>
2022-06-18 05:30:10 +0530

    added:added uc1

--- dayx/Program.cs
+++ dayx/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("welcome to employee wage computation program");
+Random random = new Random();
+int res = random.Next(0, 2);
+Console.WriteLine(res);
+if(res==1)
+{
+    Console.WriteLine("present");
+}
+else
+{
+    Console.WriteLine("absent");
+}
