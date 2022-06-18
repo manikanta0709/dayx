17c77dfebb160d2e80d2cc6ba2ea605eb285df70
manikanta0709 <manibunny0718@gmail.com>
2022-06-18 06:29:09 +0530

    added:added uc6

--- dayx/Program.cs
+++ dayx/Program.cs
@@ -4,6 +4,7 @@ Random random = new Random();
 int res = random.Next(0, 2);
 Console.WriteLine(res);
 const int wageperhour = 20, dayhour = 8, parttimehour=4,workingdaypermonth=20;
+const int workinghour = (12*8)+4;
 switch (res)
 {
     case 1:
@@ -12,8 +13,12 @@ switch (res)
         Console.WriteLine("parttime wage:" + wageperhour * parttimehour);
         Console.WriteLine("full time :" + (wageperhour * dayhour) * workingdaypermonth);
         Console.WriteLine("parttime:" + (wageperhour * parttimehour) * workingdaypermonth);
+        Console.WriteLine("total wage:" + workinghour*wageperhour);
+
         break;
     default:
         Console.WriteLine("absent");
         break;
 }
+
+
